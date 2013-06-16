498dd56950d55ee316fa84b159bee563d3e99da9
danvas <danvasq@gmail.com>
2013-06-15 21:55:21 -0700

    Changed project settings and added code/test for command line arguments. -DV

--- genData/genData.cpp
+++ genData/genData.cpp
@@ -6,18 +6,32 @@
 //
 
 #include <iostream>
-#include <ofstream>
-
+#include <fstream>
+#include <cstdlib>
 
 using namespace std;
 
-int main(int argc, const char * argv[])
+int main(int argc, const char* argv[])
 {
+    
     int numRecords;
     string fname;
+    /*
+     // For debugging (DELETE WHEN READY TO DELIVER)
+     cout << "numRecords = " << numRecords << "  " <<typeid(numRecords).name() <<endl;
+     cout << "fname = " << fname << endl;
+     cout << "fname[8] = " << fname[8] << endl;
+     cout << "fname.size() = " << fname.size() << endl;
+     
+     
+     for ( int i = 0; i < argc; i++){
+     const char* val = argv[i];
+     cout << i << ": "<< val << " " << typeid(val).name() << endl;
+     }
+     */ // debugging code
     
-    // Input values from command line (uncomment for delivery)
-//    numRecords = argv[1];
+    // Input values from command line arguments (uncomment for delivery)
+//    numRecords = atoi(argv[1]);
 //    fname = argv[2];
     
     // Default values (uncomment for testing phase)
@@ -25,45 +39,46 @@ int main(int argc, const char * argv[])
     fname = "small.txt";
     
 
+
     
     string wdir = "/Users/danvas/Google Drive/School/BCS/CPSC/221/Dev/Assignment2/";
 
-    string outfile = wdir + fname;
-    
-    string line;
-    ifstream mazefile (outfile.c_str()); // .c_str needed to compile in gcc!
-    
-    char maze[SIZE][SIZE];
-    for( int row = 0 ; row < SIZE ; row++ ){
-        for (int col = 0 ; col < SIZE ; col++ ){
-            maze[row][col] = '0'; // initialize all to null character
-            //cout << maze[row][col];
-        }
-        //cout << endl;
-    }
-    int row = 0;
-    Point start;
-    
-    if (mazefile.is_open()){ // build maze array
-        while ( mazefile.good() ){
-            getline (mazefile,line);
-            char* ptrLine = (char*)line.c_str();
-            for(int col = 0; col < line.size(); col++){
-                maze[row][col] = ptrLine[col];
-                //cout << maze[row][col];
-                if(maze[row][col] == 'S') { // get starting point
-                    start.x = row;
-                    start.y = col;
-                }
-            }
-            row++;
-            //cout << endl;
-        }
-        mazefile.close();
-    } else {
-        cout << "// File is not open. NULL return";
-        return NULL;
-    }
+//    string outfile = wdir + fname;
+//    
+//    string line;
+//    ifstream mazefile (outfile.c_str()); // .c_str needed to compile in gcc!
+//    
+//    char maze[SIZE][SIZE];
+//    for( int row = 0 ; row < SIZE ; row++ ){
+//        for (int col = 0 ; col < SIZE ; col++ ){
+//            maze[row][col] = '0'; // initialize all to null character
+//            //cout << maze[row][col];
+//        }
+//        //cout << endl;
+//    }
+//    int row = 0;
+//    Point start;
+//    
+//    if (mazefile.is_open()){ // build maze array
+//        while ( mazefile.good() ){
+//            getline (mazefile,line);
+//            char* ptrLine = (char*)line.c_str();
+//            for(int col = 0; col < line.size(); col++){
+//                maze[row][col] = ptrLine[col];
+//                //cout << maze[row][col];
+//                if(maze[row][col] == 'S') { // get starting point
+//                    start.x = row;
+//                    start.y = col;
+//                }
+//            }
+//            row++;
+//            //cout << endl;
+//        }
+//        mazefile.close();
+//    } else {
+//        cout << "// File is not open. NULL return";
+//        return NULL;
+//    }
     
     return 0;
 }
