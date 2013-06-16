3832929979921e82dbe6b24f67c89a8a9c9fe374
danvas <danvasq@gmail.com>
2013-06-16 11:48:35 -0700

    Finished implementing Part 1. Compiles in g++, but not gcc. -DV

--- genData/genData.cpp
+++ genData/genData.cpp
@@ -14,11 +14,17 @@
 #include <cxxabi.h> // for demangling/debugging
 using namespace std;
 
+string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 int main(int argc, const char* argv[])
 {
+    srand(time(0));
     
+    // Variables
+    const int numLettersInKey = 8;
     int numRecords;
-    string fname;
+    const char* filename;
+    
     /*
      // For debugging (DELETE WHEN READY TO DELIVER)
      cout << "numRecords = " << numRecords << "  " <<typeid(numRecords).name() <<endl;
@@ -39,76 +45,37 @@ int main(int argc, const char* argv[])
     
     // Default values (uncomment for testing phase)
     numRecords = 25;
-    fname = "small.txt";
+    filename = "small.txt";
     
-    int numLettersInKey = 8;
-
-
-    
-    string wdir = "/Users/danvas/Google Drive/School/BCS/CPSC/221/Dev/Assignment2/";
-    
-    string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    
-    // Loop to generate key-value pairs in a file (WIP)
-    srand(time(0));
-    for (int n = 0; n < numRecords; n++){
-        
-        // Generate key with 8 letters
-        string key;
-        for(int i = 0; i < numLettersInKey; i++) {
-            stringstream ss;
-            string s;
-            ss << letters[rand()%26];
-            ss >> s;
-            key.append(s);
+    ofstream outfile;
+    outfile.open(filename, std::ios_base::app);
+    if (outfile.is_open()) {
+        // Loop to generate key-value pairs in a file
+        for (int n = 0; n < numRecords; n++){
+            
+            // Generate key with 8 letters
+            string key;
+            for(int i = 0; i < numLettersInKey; i++) {
+                stringstream ss;
+                string s;
+                ss << letters[rand()%26];
+                ss >> s;
+                key.append(s);
+            }
+            
+            /*
+             // Print type of data (DELETE WHEN READY TO DELIVER)
+             int success;
+             char *realname = abi::__cxa_demangle(typeid(key).name(), 0, 0, &success);
+             cout << realname << endl;
+             */ // debugging code
+            
+            
+            outfile << key << "\t" << n << endl;
+            cout << n << "\t" << key << endl;
         }
-
-        /*
-        // Print type of data (DELETE WHEN READY TO DELIVER)
-        int success;
-        char *realname = abi::__cxa_demangle(typeid(key).name(), 0, 0, &success);
-        cout << realname << endl;
-        */ // debugging code
-
-        cout << n << "\t" << key << endl;
-    }
-
-//    string outfile = wdir + fname;
-//    
-//    string line;
-//    ifstream mazefile (outfile.c_str()); // .c_str needed to compile in gcc!
-//    
-//    char maze[SIZE][SIZE];
-//    for( int row = 0 ; row < SIZE ; row++ ){
-//        for (int col = 0 ; col < SIZE ; col++ ){
-//            maze[row][col] = '0'; // initialize all to null character
-//            //cout << maze[row][col];
-//        }
-//        //cout << endl;
-//    }
-//    int row = 0;
-//    Point start;
-//    
-//    if (mazefile.is_open()){ // build maze array
-//        while ( mazefile.good() ){
-//            getline (mazefile,line);
-//            char* ptrLine = (char*)line.c_str();
-//            for(int col = 0; col < line.size(); col++){
-//                maze[row][col] = ptrLine[col];
-//                //cout << maze[row][col];
-//                if(maze[row][col] == 'S') { // get starting point
-//                    start.x = row;
-//                    start.y = col;
-//                }
-//            }
-//            row++;
-//            //cout << endl;
-//        }
-//        mazefile.close();
-//    } else {
-//        cout << "// File is not open. NULL return";
-//        return NULL;
-//    }
+        outfile.close();
+    } else cout << "Error: File not opened";
     
     return 0;
 }
