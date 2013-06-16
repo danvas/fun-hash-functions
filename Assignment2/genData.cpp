c759856e7f96a85f72630a44c703032454068125
danvas <danvasq@gmail.com>
2013-06-15 18:06:35 -0700

    Added new

---
+++ Assignment2/genData.cpp
@@ -0,0 +1,9 @@
+//
+//  genData.cpp
+//  Assignment2
+//
+//  Created by Daniel Vasquez & Parham Rahmani on 2013-06-15.
+//  Copyright (c) 2013 Daniel Vasquez & Parham Rahmani. All rights reserved.
+//
+
+#include "genData.h"
